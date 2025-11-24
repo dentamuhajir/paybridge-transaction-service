3e894767be0d8c3038355f3659418b95363d787a
Denta Muhajir <denta.muhajir@binus.ac.id>
2025-11-24 22:11:13 +0700

    Add postgre config

--- internal/db/postgres.go
+++ internal/db/postgres.go
@@ -1,1 +1,22 @@
 package db
+
+import (
+	"context"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func NewPostgres(dsn string) (*pgxpool.Pool, error) {
+	ctx := context.Background()
+
+	pool, err := pgxpool.New(ctx, dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := pool.Ping(ctx); err != nil {
+		return nil, err
+	}
+
+	return pool, nil
+}
