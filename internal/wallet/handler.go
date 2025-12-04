5261fdf2e23076d5a774d43577dc35ee6a334a3b
Denta Muhajir <denta.muhajir@binus.ac.id>
2025-12-04 21:44:41 +0700

    Implement response

--- internal/wallet/handler.go
+++ internal/wallet/handler.go
@@ -3,6 +3,7 @@ package wallet
 import (
 	"net/http"
 	"paybridge-transaction-service/internal/server/middleware"
+	"paybridge-transaction-service/pkg/response"
 
 	"github.com/labstack/echo/v4"
 )
@@ -15,21 +16,29 @@ func NewHandler(svc Service) *Handler {
 	return &Handler{service: svc}
 }
 
-func (h *Handler) RegisterRoutes(g echo.Group) {
-	g.POST("/wallet/create", h.Create, middleware.ValidateInternalToken)
+func (h *Handler) RegisterRoutes(g *echo.Group) {
+	g.POST("/wallet", h.Create, middleware.ValidateInternalToken)
 }
 
 func (h *Handler) Create(c echo.Context) error {
 	var req CreateWalletReq
 
 	if err := c.Bind(&req); err != nil {
-		return c.JSON(http.StatusBadRequest, echo.Map{"error": err.Error()})
+		return c.JSON(
+			http.StatusBadRequest,
+			response.Error("invalid request body", http.StatusBadRequest),
+		)
 	}
 
 	resp, err := h.service.CreateWallet(c.Request().Context(), req)
 	if err != nil {
-		return c.JSON(http.StatusInternalServerError, echo.Map{"error": err.Error()})
+		return c.JSON(
+			http.StatusInternalServerError,
+			response.Error("failed to create wallet", http.StatusInternalServerError),
+		)
 	}
 
-	return c.JSON(http.StatusOK, resp)
+	return c.JSON(http.StatusOK,
+		response.Success("wallet created", resp, http.StatusOK),
+	)
 }
