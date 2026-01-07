963316af79f5373670e456e2afa3f9cf2d3396cb
Denta Muhajir <denta.muhajir@binus.ac.id>
2026-01-07 19:06:17 +0700

    Add getWallet repo

--- internal/domain/wallet/repository.go
+++ internal/domain/wallet/repository.go
@@ -9,6 +9,7 @@ import (
 
 type Repository interface {
 	Create(ctx context.Context, w *Wallet) (*Wallet, error)
+	Get(ctx context.Context, id string) (*Wallet, error)
 }
 
 type repository struct {
@@ -24,8 +25,7 @@ func (r *repository) Create(ctx context.Context, w *Wallet) (*Wallet, error) {
 	query := `
 		INSERT INTO wallets (user_id, currency)
 		VALUES ($1, $2)
-		RETURNING id, balance, status, created_at, updated_at
-	`
+		RETURNING id, balance, status, created_at, updated_at	`
 
 	err := r.db.QueryRow(ctx, query,
 		w.UserID,
@@ -36,6 +36,33 @@ func (r *repository) Create(ctx context.Context, w *Wallet) (*Wallet, error) {
 		&w.Status,
 		&w.CreatedAt,
 		&w.UpdatedAt,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return w, nil
+
+}
+
+func (r *repository) Get(ctx context.Context, userID string) (*Wallet, error) {
+	query := `
+		SELECT 
+			user_id, 
+			balance, 
+			currency, 
+			status 
+		FROM wallets 
+		WHERE user_id = $1
+	`
+	w := new(Wallet)
+
+	err := r.db.QueryRow(ctx, query, userID).Scan(
+		&w.UserID,
+		&w.Balance,
+		&w.Currency,
+		&w.Status,
 	)
 
 	if err != nil {
