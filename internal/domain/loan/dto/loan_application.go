99c7fba132422be5fc519852669f97a2a0cae05c
Denta Muhajir <denta.muhajir@binus.ac.id>
2026-01-12 23:10:39 +0700

    Add feature loan application dto

---
+++ internal/domain/loan/dto/loan_application.go
@@ -0,0 +1,17 @@
+package dto
+
+import "github.com/google/uuid"
+
+type LoanAppCreateReq struct {
+	UserID       uuid.UUID `json:"user_id"`
+	ProductID    uuid.UUID `json:"product_id"`
+	Amount       int64     `json:"amount"`
+	TenorMonth   int       `json:"tenor_month"`
+	InterestType string    `json:"interest_type"` // FLAT / ANNUITY
+	AdminFee     int64     `json:"admin_fee"`
+}
+
+type LoanAppCreateResp struct {
+	ID     uuid.UUID `db:"id"`
+	Status string    `db:"status"`
+}
