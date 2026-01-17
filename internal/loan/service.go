ec2bad86d66077f32f5dbd55cd666a9a362e0824
Denta Muhajir <denta.muhajir@binus.ac.id>
2026-01-17 14:24:20 +0700

    Add batch approval concurrency

--- internal/loan/service.go
+++ internal/loan/service.go
@@ -3,6 +3,7 @@ package loan
 import (
 	"context"
 	"paybridge-transaction-service/internal/loan/entity"
+	"sync"
 	"time"
 
 	"github.com/labstack/gommon/log"
@@ -12,6 +13,7 @@ import (
 type Service interface {
 	Create(ctx context.Context, req LoanAppCreateRequest) (*LoanAppCreateResponse, error)
 	Approval(ctx context.Context, req LoanApprovalRequest) (*LoanApprovalResponse, error)
+	BatchApproval(ctx context.Context, req []LoanApprovalRequest) (*LoanBatchApprovalResponse, error)
 }
 
 type service struct {
@@ -21,6 +23,64 @@ type service struct {
 
 func NewService(r Repository, log *zap.Logger) Service {
 	return &service{repo: r, log: log}
+}
+
+func (s *service) BatchApproval(ctx context.Context, req []LoanApprovalRequest) (*LoanBatchApprovalResponse, error) {
+
+	type result struct {
+		id     string
+		status string
+		err    error
+	}
+
+	resultsCh := make(chan result, len(req))
+	var wg sync.WaitGroup
+
+	for _, r := range req {
+		wg.Add(1)
+
+		go func(r LoanApprovalRequest) {
+			defer wg.Done()
+
+			resp, err := s.Approval(ctx, r)
+			if err != nil {
+				resultsCh <- result{
+					id:  r.ID.String(),
+					err: err,
+				}
+				return
+			}
+
+			resultsCh <- result{
+				id:     resp.ID,
+				status: resp.Status,
+			}
+		}(r)
+	}
+
+	wg.Wait()
+	close(resultsCh)
+
+	response := LoanBatchApprovalResponse{}
+	for r := range resultsCh {
+		item := LoanBatchApprovalResult{
+			ID:     r.id,
+			Status: r.status,
+		}
+
+		if r.err != nil {
+			item.Error = r.err.Error()
+			response.FailedCount++
+		} else {
+			response.SuccessCount++
+		}
+
+		response.Results = append(response.Results, item)
+	}
+
+	response.TotalLoanUpdate = response.SuccessCount
+
+	return &response, nil
 }
 
 func (s *service) Create(ctx context.Context, req LoanAppCreateRequest) (*LoanAppCreateResponse, error) {
